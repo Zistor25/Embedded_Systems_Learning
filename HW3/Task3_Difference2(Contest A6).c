bc505d885bb1f0bf6cc27c1b7b2500677d76e1da
Zistor25 <d.zistor@gmail.com>
2024-11-04 12:14:02 +0300

    HW2.1

---
+++ HW3/Task3_Difference2(Contest A6).c
@@ -0,0 +1,36 @@
+/*
+ * Sum3.c
+ * 
+ * Copyright 2024 ZistorPC <ZistorPC@DESKTOP-Q9IBJV9>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	int difference;
+	difference=a-b;
+	printf("%d", difference);
+	return 0;
+}
+
